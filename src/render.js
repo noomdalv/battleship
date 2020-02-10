53d3d4c5ecfc62ee142965510198f562e82524d1
noomdalv <vladlunasan@gmail.com>
2020-02-10 12:34:35 -0500

    added styling for gameboard and shipstorage

--- src/render.js
+++ src/render.js
@@ -7,25 +7,24 @@ const Render = () => {
   const playerGB = gameBoardFactory();
 
   const shipStorageDiv = document.getElementById('ship-storage-container');
-  shipStorageDiv.classList = 'col-6'
-  
+
   let currentShip;
-  
+
   const renderShipStorage = () => {
     const switchDirectionBtn = document.createElement('button');
     switchDirectionBtn.innerHTML = "V";
-    switchDirectionBtn.classList = "btn btn-primary float-right mt-2 mr-2"
+    switchDirectionBtn.classList = "btn direction-btn float-right mt-3 mr-2"
     shipStorageDiv.appendChild(switchDirectionBtn)
 
     for (let i = 1; i <= 5; i++) {
       let shipContainer = document.createElement('div');
       shipContainer.id = `ship-${i}`;
-      shipContainer.classList = 'ship m-3 border border-primary';
+      shipContainer.classList = 'ship m-3';
       shipStorageDiv.appendChild(shipContainer);
 
       shipContainer.addEventListener("click", () => {
         currentShip = playerGB.shipStorage[i];
-        switchDirectionBtn.innerHTML = currentShip.direction === 'horizontal' ? 'V': 'H';				
+        switchDirectionBtn.innerHTML = currentShip.direction === 'horizontal' ? 'V': 'H';
       })
     }
 
@@ -49,28 +48,46 @@ const Render = () => {
 
   const renderPlacementBoard = () => {
     const board = document.getElementById('player-gameboard');
-    board.classList = 'container col-6';
-    for (let i = 1; i <= 10; i++) {
+    for (let i = 0; i <= 10; i++) {
       let row = document.createElement('div');
-      row.classList = 'row';
+			if (i === 0) {
+				for (let k = 0; k <=10; k++) {
+					let cell = document.createElement('div');
+					if (k !== 0) {
+						cell.innerHTML = String.fromCharCode(64+k);
+						cell.classList = "text-center";
+					}
+					row.appendChild(cell);
+				}
+				row.classList = "letter-row cellrow"
+			} else {
+				row.classList = 'cellrow';
+			}
       board.appendChild(row);
-      for (let j = 1; j <= 10; j++) {
-        let cell = document.createElement('div');
-        cell.classList = 'col cell border border-secondary';
-        cell.setAttribute('data-x', i);
-        cell.setAttribute('data-y', j);
-        row.appendChild(cell);
-        
-        cell.addEventListener('click', () => {
-          if (!currentShip) {
-            alert('Select a ship from the left menu before!');
-          } else {
-            playerGB.placeShip(currentShip, parseInt(cell.getAttribute('data-x')), parseInt(cell.getAttribute('data-y')));
-          }          
-        })
+
+			if (i !== 0) {
+				for (let j = 0; j <= 10; j++) {
+	        let cell = document.createElement('div');
+					if (j !== 0) {
+						cell.classList = 'cellcol';
+		        cell.setAttribute('data-x', i);
+		        cell.setAttribute('data-y', j);
+					} else if (j === 0 && i !== 0){
+						cell.innerHTML = i;
+					}
+
+	        row.appendChild(cell);
+					cell.addEventListener('click', () => {
+	          if (!currentShip) {
+	            alert('Select a ship from the left menu before!');
+	          } else {
+	            playerGB.placeShip(currentShip, parseInt(cell.getAttribute('data-x')), parseInt(cell.getAttribute('data-y')));
+	          }
+	        })
+				}
       }
-    }    
-  } 
+    }
+  }
 
   return { renderShipStorage, get currentShip() { return currentShip }, renderPlacementBoard };
 
